2fa50ac07d9b1c0d9ee67d8e015bf1325894e303
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-02 12:11:10 -0400

    Updates

---
+++ precache-manifest.3386327ff05937755f876ec3c762068d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68474247bb850d0f4d5bfdbc82fc2bd",
+    "url": "/nibbl/index.html"
+  },
+  {
+    "revision": "1755ae4fae63d864b487",
+    "url": "/nibbl/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b9f46e4293f99bd882a4",
+    "url": "/nibbl/static/css/main.7688a2cf.chunk.css"
+  },
+  {
+    "revision": "1755ae4fae63d864b487",
+    "url": "/nibbl/static/js/2.dee1dab4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nibbl/static/js/2.dee1dab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f46e4293f99bd882a4",
+    "url": "/nibbl/static/js/main.66fc2556.chunk.js"
+  },
+  {
+    "revision": "7bb6a9cd7d2bea2f1dd1",
+    "url": "/nibbl/static/js/runtime-main.09239f9f.js"
+  }
+]);
